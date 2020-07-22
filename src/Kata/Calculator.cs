95b5b73d0773200b8cf52a74be845c1ac76c8963
ajzavala <zavalaalberto76@gmail.com>
2020-07-22 09:21:37 -0600

    unkwown amount of numbers

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Globalization;
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -6,7 +10,12 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(number))
                 return 0;
-            return 3;
+            
+            var numbers = number.Split(",");
+
+            if (numbers.Length>1) 
+                return Convert.ToInt32(numbers[0])+Convert.ToInt32(numbers[1]);
+            return Convert.ToInt32(numbers[0]);
         }
     }
 }
